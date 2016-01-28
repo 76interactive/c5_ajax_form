9a15f9717240b6e58b7159f406235f3232858b1c
Sjors Snoeren <hello@sjors.me>
2016-01-28 09:17:28 +0100

    Renamed View.php methods to camelCase

--- blocks/form/view.php
+++ blocks/form/view.php
@@ -1,17 +1,17 @@
 <?php defined('C5_EXECUTE') or die("Access Denied.");
 
   $formAction = $this->action('submit_form') . '#' . $qsID;
-  $ajax_url = REL_DIR_FILES_TOOLS_BLOCKS . '/form/responder';
+  $ajaxURL = REL_DIR_FILES_TOOLS_BLOCKS . '/form/responder';
 
-  $form_processing_varname = "processing_form_{$bID}";
-  $template_onsubmit_funcname = "form_{$bID}_onsubmit";
-  $template_onsuccess_funcname = "form_{$bID}_onsuccess";
-  $template_onerror_funcname = "form_{$bID}_onerror";
+  $formIsProcessingVariableName = "processing_form_{$bID}";
+  $templateOnSubmitFunctionName = "form_{$bID}_onsubmit";
+  $templateOnSuccessFunctionName = "form_{$bID}_onsuccess";
+  $templateOnErrorFunctionName = "form_{$bID}_onerror";
   
 ?>
 
   <script type="text/javascript">
-    var <?php echo $form_processing_varname; ?> = false;
+    var <?php echo $formIsProcessingVariableName; ?> = false;
 
     $(document).ready(function() {
       $('#<?php echo $formDomId; ?>').on('submit', function(e) {
@@ -26,46 +26,46 @@
           formData[pair.name] = pair.value;
         });
 
-        if (<?php echo $form_processing_varname; ?>) {
+        if (<?php echo $formIsProcessingVariableName; ?>) {
           return false; //prevent re-submission while waiting for response
         }
-        <?php echo $form_processing_varname; ?> = true;
-        <?php echo $template_onsubmit_funcname; ?>('#<?php echo $formDomId; ?>');
+
+        <?php echo $formIsProcessingVariableName; ?> = true;
+        <?php echo $templateOnSubmitFunctionName; ?>('#<?php echo $formDomId; ?>');
 
         $.ajax({
-          'url': '<?php echo $ajax_url; ?>',
+          'url': '<?php echo $ajaxURL; ?>',
           'dataType': 'json',
           'data': formData,
           'type': 'POST',
           'success': function(response) {
-            <?php echo $form_processing_varname; ?> = false;
+            <?php echo $formIsProcessingVariableName; ?> = false;
+
             if (response.success) {
               $('#<?php echo $formDomId; ?>').clearForm();
-              <?php echo $template_onsuccess_funcname; ?>('#<?php echo $formDomId; ?>', response.message);
+              <?php echo $templateOnSuccessFunctionName; ?>('#<?php echo $formDomId; ?>', response.message);
             } else {
-              <?php echo $template_onerror_funcname; ?>('#<?php echo $formDomId; ?>', response.message);
+              <?php echo $templateOnErrorFunctionName; ?>('#<?php echo $formDomId; ?>', response.message);
             }
           }
         });
 
       });
     });
-  </script>
-    
-  <script type="text/javascript">
-    function <?php echo $template_onsubmit_funcname; ?>(form) {
+
+    function <?php echo $templateOnSubmitFunctionName; ?>(form) {
       $(form).find('.errors').hide().html('');
       $(form).find('input[type=submit]').hide();
       $(form).find('.indicator').show();
     }
 
-    function <?php echo $template_onsuccess_funcname; ?>(form, thanks) {
+    function <?php echo $templateOnSuccessFunctionName; ?>(form, thanks) {
       $(form).find('.success').html(thanks).show();
       $(form).find('.indicator').hide();
       $(form).find('.fields').hide();
     }
 
-    function <?php echo $template_onerror_funcname; ?>(form, message) {
+    function <?php echo $templateOnErrorFunctionName; ?>(form, message) {
       $(form).find('.indicator').hide();
       $(form).find('input[type=submit]').show();
       $(form).find('.errors').html(message).show();
@@ -73,56 +73,56 @@
   </script>
 
   <div id="formblock<?php echo $bID; ?>" class="formblock">
-  <form id="<?php echo $formDomId; ?>" method="post" action="<?php echo $formAction; ?>" <?php echo ($hasFileUpload ? 'enctype="multipart/form-data"' : ''); ?>>
+    <form id="<?php echo $formDomId; ?>" method="post" action="<?php echo $formAction; ?>" <?php echo ($hasFileUpload ? 'enctype="multipart/form-data"' : ''); ?>>
 
-    <div class="success" <?php echo !$success ? 'style="display: none;"' : ''; ?>>
-      <?php echo $thanksMsg; ?>
-    </div>
-    
-    <div class="errors" <?php echo !$errors ? 'style="display: none;"' : ''; ?>>
-      <?php echo $errorHeader; ?>
-      <ul>
+      <div class="success" <?php echo !$success ? 'style="display: none;"' : ''; ?>>
+        <?php echo $thanksMsg; ?>
+      </div>
+      
+      <div class="errors" <?php echo !$errors ? 'style="display: none;"' : ''; ?>>
+        <?php echo $errorHeader; ?>
+        <ul>
+          <?php
+            if (isset($errors) && count($errors) > 0) {
+              foreach ($errors as $error) {
+          ?>
+          <li><?php echo $error; ?></li>
+          <?php
+              }
+            }
+          ?>
+        </ul>
+      </div>
+
+      <div class="fields">
         <?php
-          if (isset($errors) && count($errors) > 0) {
-            foreach ($errors as $error) {
+          if (isset($questions) && count($questions) > 0) {
+            foreach ($questions as $question) {
         ?>
-        <li><?php echo $error; ?></li>
+          <div class="field field-<?php echo $question['type']; ?>">
+            <label <?php echo $question['labelFor']; ?> class="<?php echo $question['labelClasses']; ?>">
+              <?php echo $question['question']; ?>
+              <?php if ($question['required']): ?>
+                <span class="required">*</span>
+              <?php endif; ?>
+            </label>
+
+            <?php echo $question['input']; ?>
+          </div>
         <?php
             }
           }
         ?>
-      </ul>
-    </div>
+      </div><!-- .fields -->
 
-    <div class="fields">
-      <?php
-        if (isset($questions) && count($questions) > 0) {
-          foreach ($questions as $question) {
-      ?>
-        <div class="field field-<?php echo $question['type']; ?>">
-          <label <?php echo $question['labelFor']; ?> class="<?php echo $question['labelClasses']; ?>">
-            <?php echo $question['question']; ?>
-            <?php if ($question['required']): ?>
-              <span class="required">*</span>
-            <?php endif; ?>
-          </label>
+      <input type="submit" name="Submit" class="submit" value="Submit" />
 
-          <?php echo $question['input']; ?>
-        </div>
-      <?php
-          }
-        }
-      ?>
-    </div><!-- .fields -->
+      <div class="indicator" style="display: none;">
+        <img src="<?php echo ASSETS_URL_IMAGES; ?>/throbber_white_16.gif" width="16" height="16" alt="" />
+        <span>Processing...</span>
+      </div>
 
-    <input type="submit" name="Submit" class="submit" value="Submit" />
-
-    <div class="indicator" style="display: none;">
-      <img src="<?php echo ASSETS_URL_IMAGES; ?>/throbber_white_16.gif" width="16" height="16" alt="" />
-      <span>Processing...</span>
-    </div>
-
-    <input name="qsID" type="hidden" value="<?php echo $qsID; ?>" />
-    <input name="pURI" type="hidden" value="<?php echo $pURI; ?>" />
-  </form>
+      <input name="qsID" type="hidden" value="<?php echo $qsID; ?>" />
+      <input name="pURI" type="hidden" value="<?php echo $pURI; ?>" />
+    </form>
   </div><!-- .formblock -->
